9d76bc37d0277da97c4b1d47406faae51a7814f2
Pedro Muniz <joao.munnyz@gmail.com>
2025-01-02 19:12:22 -0300

    array

---
+++ aulas/array/index.js
@@ -0,0 +1,9 @@
+var item1 = 'arroz' ;
+var item2 = 'feijao' ;
+var item3 = 'leite' ; 
+var item4 = 'macarrao';
+
+const lista = ['arroz',"feijao","leite","macarrao"]
+
+let x = lista[3]
+console.log(lista)
---
+++ aulas/eventos/index.js
@@ -0,0 +1,20 @@
+function start() {
+    document.body.style.backgroundColor = 'gray';
+}
+function dblclick(){
+    document.body.style.backgroundColor = 'green';
+}
+function entrou(){
+    document.getElementById('teste').style.backgroundColor = 'green';
+}
+function saiu(){
+    document.getElementById('teste').style.backgroundColor ='black';
+}
+function clicou(){
+    alert('clicou')
+}
+function focado(){
+    let texto1 = document.getElementById('txt1');
+    texto1.value = ''
+    
+}
