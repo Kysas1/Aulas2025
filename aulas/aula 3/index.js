5781f6668a142c33a47bbe4ab99d04d32bd53cc7
Pedro Muniz <joao.munnyz@gmail.com>
2025-01-02 01:44:40 -0300

    aulas

---
+++ aulas/aula 3/index.js
@@ -0,0 +1,11 @@
+var valor1,valor2,total ;
+valor1 = 5 ;
+valor2 = 2 ;
+
+total = valor1 + valor2 ;
+alert(total)
+
+var idade , eleitor;
+idade = 15;
+eleitor  = (idade < 18) ? "Não Eleitor" : "Eleitor";
+alert(`A resposta é ${eleitor}`)
