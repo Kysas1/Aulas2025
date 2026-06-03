f2cbaa2b4b19928ae4c6f092792ec82db0c99a90
Pedro Muniz <joao.munnyz@gmail.com>
2026-06-03 17:25:28 -0300

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -3,4 +3,16 @@ const numbersx2 = numbers.map(function(x2){
     return x2 * 2
 })
 
-console.log(numbersx2)
+console.log(numbersx2);
+const idades = [2,18,22,35,56];
+//const idadespares = idades.filter(function(par){
+//    return par % 2 === 0 
+//})
+
+//console.log(idadespares)
+
+ const idadesreduce = idades.reduce(function(reduced , acumulator){
+    return reduced + acumulator
+ },0);
+
+ console.log(idadesreduce);
